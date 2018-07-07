90a6661f3c2755b2ec4a53389e9fe01fb57f16db
MaoXi <37741439+MaoXi@users.noreply.github.com>
2018-07-07 14:11:21 +0800

    Add files via upload

---
+++ login_1/dialog.h
@@ -0,0 +1,30 @@
+﻿#ifndef DIALOG_H
+#define DIALOG_H
+
+#include "ui_dialog.h"
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = 0);
+    ~Dialog();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::Dialog *ui;
+};
+
+#endif // DIALOG_H
