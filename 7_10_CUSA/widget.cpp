0203db963afd51c6a6729162739c935e01da6a51
MaoXi <37741439+MaoXi@users.noreply.github.com>
2018-07-11 09:10:44 +0800

    2
    CUSA

---
+++ 7_10_CUSA/widget.cpp
@@ -0,0 +1,47 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    ui->tabWidget->setCurrentIndex(0);
+    QWidget *widget1 = new QWidget;
+    ui->tabWidget->addTab(widget1,"test");
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::changeEvent(QEvent *e)
+{
+    QWidget::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
+
+void Widget::on_pushButton_2_clicked()
+{
+    ui->tabWidget->setCurrentIndex(1);
+}
+
+
+void Widget::on_spinBox_editingFinished()
+{
+    int a;
+    a=ui->spinBox->value();
+
+    ui->comboBox->clear();
+    for(int i=1;i<=a;i++)
+    {
+        ui->comboBox->addItem(QString::number(i));
+    }
+}
---
+++ 7_10_CUSA/widget.h
@@ -0,0 +1,32 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private slots:
+    void on_pushButton_2_clicked();
+
+
+
+    void on_spinBox_editingFinished();
+
+private:
+    Ui::Widget *ui;
+};
+
+#endif // WIDGET_H
